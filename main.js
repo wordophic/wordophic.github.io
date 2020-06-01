ec0c737dbfbe663260dfbd1fb9a088cb3fe4d167
wordophic <65862531+wordophic@users.noreply.github.com>
2020-06-02 01:25:37 +0500

    Learning purpose

---
+++ main.js
@@ -0,0 +1,49 @@
+const responsive={
+    0:{
+        items:1
+    },
+    
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    }
+
+}
+$(document).ready(function(){
+
+    $nav=$('.nav');
+    $toggleCollapse=$('.toggle-collapse');
+    /** click event on toggle menu */
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');
+    })
+
+
+    // owl crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:true,
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+        responsive:responsive
+    });
+
+
+    // click to scroll up
+    $('.move-up span').click(function(){
+        $('html,body').animate({
+            scrollTop:0
+        },1000);
+    })
+
+    //AOS on scroll instance
+    AOS.init();
+
+});
