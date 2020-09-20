cbbbd3b623e808a9e04c84e0452afb2ea1b6b8b1
Adnan Khan <42301456+Adnan-Khan7@users.noreply.github.com>
2020-09-20 13:29:47 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
         autoplay:true,
-        autoplayTimeout:3000,
+        autoplayTimeout:9000,
         dots:false,
         nav:true,
         navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
