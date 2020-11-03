b57e89f215181583e3a17cf3c36df0ade0fb73fc
Adnan Khan <42301456+Adnan-Khan7@users.noreply.github.com>
2020-11-03 06:20:30 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -47,3 +47,20 @@ $(document).ready(function(){
     AOS.init();
 
 });
+
+Share = {
+    facebook: function(purl, ptitle, pimg, text) {
+    url = 'http://www.facebook.com/sharer.php?s=100';
+    url += '&p[title]=' + encodeURIComponent(ptitle);
+    url += '&p[summary]=' + encodeURIComponent(text);
+    url += '&p[url]=' + encodeURIComponent(purl);
+    url += '&p[images][0]=' + encodeURIComponent(pimg);
+    Share.popup(url);
+    },
+    popup: function(url) {
+        window.open(url,'','toolbar=0,status=0,width=626, height=436');
+        }
+        };
+        
+
+        
